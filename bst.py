4c195ba89706f5699bdb44d552e39e39fc285a56
Jimmy <jmart792@calpoly.edu>
2025-05-16 23:01:23 -0700

    Made data definitions and finished functions 1-2

--- bst.py
+++ bst.py
@@ -4,6 +4,47 @@ from typing import *
 from dataclasses import dataclass
 sys.setrecursionlimit(10**6)
 
-
 def example_fun(x : int) -> bool:
     return x < 142
+
+## Data Definitions
+BinTree = Union['Node', None]
+
+@dataclass(frozen=True)
+class Node:
+    element: Any
+    right: BinTree
+    left: BinTree
+
+@dataclass(frozen=True)
+class frozenBinarySearchTree:
+    comes_before: Callable[[Any, Any], bool]
+    tree: BinTree
+
+# Function 1
+# PS: Return True if BinarySearchTree is empy, otherwise False
+def is_empty(b: BinTree) -> bool:
+    if b is None:
+        return True
+    else:
+        return False
+
+# Function 2
+# PS: Add a value to a BST using comes_before func. and returns the resulting Tree
+def insert(bst: frozenBinarySearchTree, value: Any) -> frozenBinarySearchTree:
+    def insert_helper(b: BinTree, v: Any, comes_before: Callable[[Any, Any], bool]) -> BinTree:
+        if b is None:
+            return Node(v, None, None)
+        if comes_before(v, b.element):
+            left = insert_helper(b.left, v, comes_before)
+            return Node(b.element, left, b.right)
+        else:
+            right = insert_helper(b.right, v, comes_before)
+            return Node(b.element, b.left, right)
+
+    new_tree = insert_helper(bst.tree, value, bst.comes_before)
+    return frozenBinarySearchTree(bst.comes_before, new_tree)
+
+
+
+
--- bst_tests.py
+++ bst_tests.py
@@ -11,5 +11,5 @@ class BSTTests(unittest.TestCase):
         self.assertEqual(True, example_fun(34))
         self.assertEqual(False,example_fun(1423))
 
-if (__name__ == '__main__'):
+if __name__ == '__main__':
     unittest.main()
