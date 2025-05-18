d7368c7082c34246dbde736b90ade18182a3c315
cesarvazquez-69 <cesar60vazquez@gmail.com>
2025-05-17 22:11:30 -0700

    Created Function 3 & 4; Not sure if they're correct. Needs checking

--- bst.py
+++ bst.py
@@ -45,6 +45,52 @@ def insert(bst: frozenBinarySearchTree, value: Any) -> frozenBinarySearchTree:
     new_tree = insert_helper(bst.tree, value, bst.comes_before)
     return frozenBinarySearchTree(bst.comes_before, new_tree)
 
+# -- 
+
+# Function #3 
+# PS: 
+def lookup(bst: frozenBinarySearchTree, value: Any) -> bool:
+    def lookup_helper(b: BinTree, v: Any, comes_before: Callable[[Any, Any], bool]) -> bool:
+        if b is None:
+            return False
+        if not comes_before(v, b.element) and not comes_before(b.element, v): 
+            return True
+        if comes_before(v, b.element):
+            return lookup_helper(b.left, v, comes_before)
+        else:
+            return lookup_helper(b.right, v, comes_before)
+    return lookup_helper(bst.tree, value, bst.comes_before)
 
 
+# Function 4
+# PS: 
+def find_min(b: BinTree) -> Any: 
+    if b.left is None:
+        return b.element
+    return find_min(b.left)
+
+
+
+def delete(bst: frozenBinarySearchTree, value: Any) -> frozenBinarySearchTree:
+    def delete_helper(b: BinTree, v: Any, comes_before: Callable[[Any, Any], bool]) -> BinTree:
+        if b is None: 
+            return None
+        if not comes_before(v, b.element) and not comes_before(b.element, v):
+            if b.left is None:
+                return b.right
+            if b.right is None: 
+                return b.left
+            
+            Tree = find_min(b.right)
+            right = delete_helper(b.right, Tree, comes_before)
+            return Node(Tree, b.left, b.right)
+        elif comes_before(b, b.element):
+            left = delete_helper(b.left, v, comes_before)
+            return Node(b.element, left, b.right)
+        else:
+            right = delete_helper(b.right, v, comes_before)
+            return Node(b.element, b.left, right)
+        
+    new_tree = delete_helper(bst.tree, value, bst.comes_before)
+    return frozenBinarySearchTree(bst.comes_before, new_tree)
 
